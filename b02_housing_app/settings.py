a54c9fc5c291ef31e79d154be6ecb2beba747cb9
bejoysen-uva <bejoysen.math@gmail.com>
2021-10-03 18:58:59 -0400

    Step 3 of Oauth tutorial

--- b02_housing_app/settings.py
+++ b02_housing_app/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'b02_housing_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
