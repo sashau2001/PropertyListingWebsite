e715a86c4440aec58e3d0a2d556b3cdffbdde180
bejoysen-uva <bejoysen.math@gmail.com>
2021-10-03 19:19:23 -0400

    Finished Oauth tutorial

--- b02_housing_app/settings.py
+++ b02_housing_app/settings.py
@@ -135,7 +135,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Google Login
-SITE_ID = 2
+# SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
