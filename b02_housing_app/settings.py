d62c43264e01fe0e774e93a999f6af80cac4ee94
bejoysen-uva <bejoysen.math@gmail.com>
2021-10-18 16:56:27 -0400

    Check whether SITE_ID even exists

--- b02_housing_app/settings.py
+++ b02_housing_app/settings.py
@@ -144,7 +144,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Google Login
-SITE_ID = 2
+# SITE_ID = 2
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
@@ -169,3 +169,5 @@ AUTHENTICATION_BACKENDS = [
 if 'HEROKU' in os.environ:
     django_heroku.settings(locals())
     SITE_ID = 6
+
+SITE_ID/3
