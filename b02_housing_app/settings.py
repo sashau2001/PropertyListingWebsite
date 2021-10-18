d8c86d821dcb1eecd55f4f2d83f65194d07d3358
bejoysen-uva <bejoysen.math@gmail.com>
2021-10-18 17:07:28 -0400

    Try Heroku SITE_ID=4

--- b02_housing_app/settings.py
+++ b02_housing_app/settings.py
@@ -144,7 +144,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Google Login
-SITE_ID = 3
+SITE_ID = 4
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
