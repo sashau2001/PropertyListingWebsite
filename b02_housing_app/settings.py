76794ef594b6b7008d368efeb461d98023138623
bejoysen-uva <bejoysen.math@gmail.com>
2021-10-18 16:30:22 -0400

    Updated test database to sqlite3

--- b02_housing_app/settings.py
+++ b02_housing_app/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import os
+import os, sys
 import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -80,7 +80,6 @@ WSGI_APPLICATION = 'b02_housing_app.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -93,6 +92,11 @@ DATABASES = {
     }
 }
 
+if 'test' in sys.argv:
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
