811b033565f9029279a1e1c8bf6daf2ed58143d2
Hannah Han <Hannah-Han@users.noreply.github.com>
2021-10-30 12:05:24 -0400

    Update models.py
    limited rating stars to values between 1-5

--- b02_housing_app/models.py
+++ b02_housing_app/models.py
@@ -18,7 +18,14 @@ class Review(models.Model):
     apt_name = models.ForeignKey(Apartment, on_delete=models.CASCADE)
     apt_reviewer = models.CharField(max_length=100)
     apt_review = models.TextField()
-    apt_stars = models.IntegerField(default=5)
+    # apt_stars = models.IntegerField(default=5)
+    class Stars(models.IntegerChoices):
+        ONE = 1
+        TWO = 2
+        THREE = 3
+        FOUR = 4
+        FIVE = 5
+    apt_stars = models.IntegerField(choices=Stars.choices)
 
     def __str__(self):
         return "\"{}\" by \"{}\"".format(self.apt_name,self.apt_reviewer)
