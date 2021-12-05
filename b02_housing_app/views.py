b75fb6610da8cdae749080f5e3067708fa58b88e
Matthew Fruchterman <mjf8jf@virginia.edu>
2021-12-05 14:07:12 -0500

    working on paging

--- b02_housing_app/views.py
+++ b02_housing_app/views.py
@@ -4,7 +4,8 @@ from django.conf import settings
 from .forms import *
 from django.contrib import messages
 import requests,json
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.views.generic.list import ListView
 
 
 
@@ -60,10 +61,20 @@ def review(request,pk):
     context = {'review': this_review, 'reviews': True}
     return render(request,'review.html',context)
 
+
+
 def apartments(request):
     apartment_list = Apartment.objects.all()
-    p = Paginator(apartment_list, 8)
-    page_obj = p.page(1)
+    p = Paginator(apartment_list, 5)
+    page_number = request.GET.get('page')
+    try:
+        page_obj = p.get_page(page_number)  # returns the desired page object
+    except PageNotAnInteger:
+        # if page_number is not an integer then assign the first page
+        page_obj = p.page(1)
+    except EmptyPage:
+        # if page is empty then return last page
+        page_obj = p.page(p.num_pages)
 
     context = {'apartment_list': apartment_list, 'apartments': True, 'page_obj': page_obj}
     return render(request, 'apartments.html', context)
@@ -137,3 +148,9 @@ def get_distance(source,dest):
         return req.json()['rows'][0]['elements'][0]['distance']['value']
     except:
         return -1 # invaid address?
+
+
+
+class ApartmentListView(ListView):
+    model = Apartment
+    paginate_by = 5
