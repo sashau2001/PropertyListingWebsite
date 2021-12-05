168d5439f27cd751a9a6c2f9f2dc08a90162d13e
Hannah Han <Hannah-Han@users.noreply.github.com>
2021-12-05 13:50:24 -0500

    Update models.py

--- b02_housing_app/models.py
+++ b02_housing_app/models.py
@@ -2,10 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-# from cloudinary.models import CloudinaryField
-
-# class Photo(models.Model):
-#   image = CloudinaryField('image')
 
 class CustomTemplate:
     field_dict = {}
